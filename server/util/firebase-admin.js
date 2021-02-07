b8d9989977c26138947f352ec6e72fa50b61efc4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-02-07 11:08:20 +0530

    commit

--- server/util/firebase-admin.js
+++ server/util/firebase-admin.js
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
       project_id: process.env.FIREBASE_PROJECT_ID,
     }),
   });
