e6b11718de7c2849bf107043ca1d9ffdda0a0232
faisalsayed10 <faisal.sayed502@gmail.com>
2021-02-07 12:36:22 +0530

    fixing useEffect

--- client/src/pages/PrivateRoute.js
+++ client/src/pages/PrivateRoute.js
@@ -2,12 +2,10 @@ import React, { useContext, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../schema/queries";
-import { UserContext } from "../context/context";
 import Loading from "../components/Loading";
 
 function PrivateRoute({ children, ...rest }) {
   const { data, loading } = useQuery(GET_USER, { fetchPolicy: 'cache-and-network'});
-  const { setUser } = useContext(UserContext)
 
   console.log("logged")
 
