0e268acaf2318086107f9c46bd9df8b7f610b7c2
faisalsayed10 <faisal.sayed502@gmail.com>
2021-02-07 12:45:24 +0530

    fixing request cancelled

--- client/src/pages/PrivateRoute.js
+++ client/src/pages/PrivateRoute.js
@@ -2,14 +2,21 @@ import React, { useContext, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../schema/queries";
+import { UserContext } from "../context/context";
 import Loading from "../components/Loading";
 
 function PrivateRoute({ children, ...rest }) {
   const { data, loading } = useQuery(GET_USER, { fetchPolicy: 'cache-and-network'});
+  const { setUser } = useContext(UserContext)
 
   console.log("logged")
 
-  if (loading && !data) return <Loading />;
+  useEffect(() => {
+    setUser(data?.me)
+    //eslint-disable-next-line
+  }, [data])
+
+  if (loading) return <Loading />;
   if (!data?.me?.id) {
     return <Redirect to="/login" />;
   }
